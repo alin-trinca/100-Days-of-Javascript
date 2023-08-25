4b2a57cfcdc6bc8ae59689871f3a5f7fb9157d50
Alin Trinca <contact@garin.ro>
2023-08-25 09:08:00 +0300

    Javascript Emoji Creator

---
+++ 045 Javascript Emoji Creator/README.md
@@ -0,0 +1,5 @@
+# 045 Javascript Emoji Creator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/alin_trinca/pen/bGOGxvw](https://codepen.io/alin_trinca/pen/bGOGxvw).
+
+![Javascript Emoji Creator Screenshot](random-emoji-creator.jpg)
---
+++ 045 Javascript Emoji Creator/dist/script.js
@@ -0,0 +1,132 @@
+const emojis = [
+  "😀",
+  "😃",
+  "😄",
+  "😁",
+  "😆",
+  "😅",
+  "🤣",
+  "😂",
+  "🙂",
+  "🙃",
+  "😉",
+  "😊",
+  "😇",
+  "🥰",
+  "😍",
+  "🤩",
+  "😘",
+  "😗",
+  "😚",
+  "😙",
+  "😋",
+  "😛",
+  "😝",
+  "😜",
+  "🤪",
+  "🤨",
+  "🧐",
+  "🤓",
+  "😎",
+  "🤩",
+  "🥳",
+  "😏",
+  "😒",
+  "😞",
+  "😔",
+  "😟",
+  "😕",
+  "🙁",
+  "☹️",
+  "😣",
+  "😖",
+  "😫",
+  "😩",
+  "🥺",
+  "😢",
+  "😭",
+  "😤",
+  "😠",
+  "😡",
+  "🤬",
+  "🤯",
+  "😳",
+  "🥵",
+  "🥶",
+  "😱",
+  "😨",
+  "😰",
+  "😥",
+  "😓",
+  "🤗",
+  "🤔",
+  "🤭",
+  "🤫",
+  "🤥",
+  "😶",
+  "😐",
+  "😑",
+  "😬",
+  "🙄",
+  "😯",
+  "😦",
+  "😧",
+  "😮",
+  "😲",
+  "😴",
+  "🤤",
+  "😪",
+  "😵",
+  "🤐",
+  "🥴",
+  "🤢",
+  "🤮",
+  "🤕",
+  "🥳",
+  "😷",
+  "🤒",
+  "🤕",
+  "🤑",
+  "🤠",
+  "😈",
+  "👿",
+  "👹",
+  "👺",
+  "💀",
+  "👻",
+  "👽",
+  "🤖",
+  "💩",
+  "😺",
+  "😸",
+  "😹",
+  "😻",
+  "😼",
+  "😽",
+  "🙀",
+  "😿",
+  "😾"
+];
+
+function getRandomEmoji() {
+  let emoji = emojis[Math.floor(Math.random() * emojis.length)];
+  document.getElementById("emoji-container").textContent = emoji;
+}
+
+const emojiContainer = document.getElementById("emoji-container");
+const generateButton = document.getElementById("generate-button");
+
+generateButton.addEventListener("click", () => {
+  getRandomEmoji();
+  // Add random animation class and remove it after a duration
+  const animationClasses = ["smile", "pulsate"];
+  const randomAnimation =
+    animationClasses[Math.floor(Math.random() * animationClasses.length)];
+
+  emojiContainer.classList.add(randomAnimation);
+  setTimeout(() => {
+    emojiContainer.classList.remove(randomAnimation);
+  }, 1500); // Adjust the duration based on the animation type
+});
+
+getRandomEmoji();
