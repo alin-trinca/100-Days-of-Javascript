b6c98ec4c1b27c8c33833a6c391393843f4e7c68
Alin Trinca <contact@garin.ro>
2023-09-15 09:18:38 +0300

    Javascript Typing Effect

---
+++ 066 Javascript Typing Effect/README.md
@@ -0,0 +1,5 @@
+# 066 Javascript Typing Effect
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/alin_trinca/pen/qBLmKag](https://codepen.io/alin_trinca/pen/qBLmKag).
+
+![Javascript Typing Effect Screenshot](javascript-typing-effect.png)
---
+++ 066 Javascript Typing Effect/src/script.js
@@ -0,0 +1,32 @@
+const dynamicText = document.querySelector("h1 span");
+const words = ["Fun", "Hard", "Not Enough", "The Best"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    // If condition is true, type the next character
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    // If condition is true, remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    // If word is deleted then switch to the next word
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+
+typeEffect();
